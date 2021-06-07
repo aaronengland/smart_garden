95c699319b45ff3c28bce9d1e01a4b9824c36269
aaronengland <aaron.england.dev@gmail.com>
2021-06-06 19:57:24 -0600

    logging to dictionary

--- script.py
+++ script.py
@@ -1,12 +1,21 @@
 from functions import Interval_Watering
 import features as feat
 
-# initialize class
-cls_interval_watering = Interval_Watering()
-
-# water
-cls_interval_watering.watering(int_channel_input=feat.int_channel_input, 
-                               int_channel_output=feat.int_channel_output, 
-                               int_sec_sleep_dry=feat.int_sec_sleep_dry, 
-                               int_sec_sleep_wet=feat.int_sec_sleep_wet,
-                               str_filename=feat.str_filename)
+# run code
+if __name__ == '__main__':
+    # initialize class
+    try:
+        cls_interval_watering = Interval_Watering()
+        print('Successful initialization of Smart Garden.')
+    except:
+        print('Error initializing Smart Garden.')
+    # water
+    try:
+        cls_interval_watering.watering(int_channel_input=feat.int_channel_input, 
+                                       int_channel_output=feat.int_channel_output, 
+                                       int_sec_sleep_dry=feat.int_sec_sleep_dry, 
+                                       int_sec_sleep_wet=feat.int_sec_sleep_wet,
+                                       str_filename=feat.str_filename)
+        print('Running Smart Garden...')
+    except:
+        print('Error running Smart Garden.')
