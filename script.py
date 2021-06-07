058990a53305ec201bd544cb3f786db4fc96dd44
aaronengland <aaron.england.dev@gmail.com>
2021-06-06 19:36:30 -0600

    converted to class

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
-from functions import interval_watering
+from functions import Interval_Watering
 import features as feat
 
-# run function
-interval_watering(int_channel_input=feat.int_channel_input, 
-                  int_channel_output=feat.int_channel_output, 
-                  int_sec_sleep_dry=feat.int_sec_sleep_dry, 
-                  int_sec_sleep_wet=feat.int_sec_sleep_wet)
+# initialize class
+cls_interval_watering = Interval_Watering()
+
+# water
+cls_interval_watering.watering(int_channel_input=feat.int_channel_input, 
+                               int_channel_output=feat.int_channel_output, 
+                               int_sec_sleep_dry=feat.int_sec_sleep_dry, 
+                               int_sec_sleep_wet=feat.int_sec_sleep_wet)
