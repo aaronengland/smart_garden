3ee23cb04aa7aeeb25eaa273313004bb8d73e979
aaronengland <aaron.england.dev@gmail.com>
2021-06-06 19:18:56 -0600

    created functions.py

---
+++ features.py
@@ -0,0 +1,10 @@
+# features
+
+# input GPIO pin
+int_channel_input = 40
+# output GPIO pin
+int_channel_output = 15
+# seconds to wait if soil dry
+int_sec_sleep_dry = 1
+# seconds to wait of soil is wet
+int_sec_sleep_wet = 10
--- script.py
+++ script.py
@@ -1,18 +1,8 @@
 from functions import interval_watering
-
-# https://sourceforge.net/p/raspberry-gpio-python/wiki/Examples/
-
-# input GPIO pin
-int_channel_input = 40
-# output GPIO pin
-int_channel_output = 15
-# seconds to wait if soil dry
-int_sec_sleep_dry = 1
-# seconds to wait of soil is wet
-int_sec_sleep_wet = 10
+import features as feat
 
 # run function
-interval_watering(int_channel_input=int_channel_input, 
-                  int_channel_output=int_channel_output, 
-                  int_sec_sleep_dry=int_sec_sleep_dry, 
-                  int_sec_sleep_wet=int_sec_sleep_wet)
+interval_watering(int_channel_input=feat.int_channel_input, 
+                  int_channel_output=feat.int_channel_output, 
+                  int_sec_sleep_dry=feat.int_sec_sleep_dry, 
+                  int_sec_sleep_wet=feat.int_sec_sleep_wet)
